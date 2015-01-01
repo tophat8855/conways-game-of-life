cf5285655cee9111f0ba97cd20e7936c7f57697e
tophat8855 <hrfarley@gmail.com>
2014-12-31 18:17:21 -0800

    sleep

--- game.rb
+++ game.rb
@@ -4,12 +4,8 @@ require "./lib/cell"
 board = Conways::Board.new
 board.seed
 
-puts "Ready? Type 'quit' to exit"
-puts "Keep pressing 'return' to go to next stage"
-continue = gets.chomp
-
-while continue != 'quit'
+while true
+  sleep(0.3)
   board.print_board
-  continue = gets.chomp
   board.ready_board
 end
